310f163d918cfffdffc6c7c5b967d85bf15a9216
joeun-01 <joeun2187@gmail.com>
2022-07-03 00:01:02 +0900

    add : [MainActivity] 바텀 네비게이션 구현 중

--- app/src/main/java/com/example/eraofband/MainActivity.kt
+++ app/src/main/java/com/example/eraofband/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.eraofband
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.eraofband.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottomNavigationView.background = null
 
         // test commit
     }
