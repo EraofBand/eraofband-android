cec488bd95ba29dd2393c519b31217fb9e2ed5e5
Blue <cha-conne@naver.com>
2022-07-02 16:54:36 +0900

    Test commit

--- app/src/main/java/com/example/eraofband/MainActivity.kt
+++ app/src/main/java/com/example/eraofband/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.eraofband
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//test commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
