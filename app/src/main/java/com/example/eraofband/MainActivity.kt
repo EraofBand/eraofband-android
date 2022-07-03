8479e9c0497448312b347be1aca5088fcebca21d
joeun-01 <joeun2187@gmail.com>
2022-07-04 03:55:04 +0900

    modify : [MainActivity] 바텀 네비게이션 여전히 구현 중 2

--- app/src/main/java/com/example/eraofband/MainActivity.kt
+++ app/src/main/java/com/example/eraofband/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.eraofband
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.eraofband.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +19,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        binding.mainBottomNav.background = null
+        // 바텀 네비게이션 모서리 둥글게 만들기
+        val bottomNavBg = binding.mainBottomNav.background as MaterialShapeDrawable
+        bottomNavBg.shapeAppearanceModel = bottomNavBg.shapeAppearanceModel.toBuilder()
+            .setTopLeftCorner(CornerFamily.ROUNDED, 70.0f)
+            .setTopRightCorner(CornerFamily.ROUNDED, 70.0f).build()
 
     }
 }
