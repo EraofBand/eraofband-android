1e491b63ef7f383589cde3b9dd60608697ae7ab5
lsh9295 <lsh929500@gmail.com>
2022-07-04 16:29:55 +0900

    add: 카카오 로그인 implementation / 키해시 구하기

--- app/src/main/java/com/example/eraofband/LoginActivity.kt
+++ app/src/main/java/com/example/eraofband/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.example.eraofband
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.kakao.sdk.common.util.Utility
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("Hash", keyHash)
     }
 }
