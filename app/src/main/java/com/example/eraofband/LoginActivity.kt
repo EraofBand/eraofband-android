056f33f66941959cfe22017ba3f85021fa602a8b
joeun-01 <joeun2187@gmail.com>
2022-07-04 19:09:45 +0900

    modify : [MainActivity] 바텀 네비게이션 아마도 구현 끝일수도

--- app/src/main/java/com/example/eraofband/LoginActivity.kt
+++ app/src/main/java/com/example/eraofband/LoginActivity.kt
@@ -1,15 +1,18 @@
 package com.example.eraofband
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.kakao.sdk.common.util.Utility
+import com.example.eraofband.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val keyHash = Utility.getKeyHash(this)
-        Log.d("Hash", keyHash)
+
     }
 }
