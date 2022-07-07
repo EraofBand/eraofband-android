aebc31a575ac2350819d2260bd36836fd37cce85
lsh9295 <lsh929500@gmail.com>
2022-07-07 17:01:10 +0900

    refactor: 코드 리팩토링

--- app/src/main/java/com/example/eraofband/signup/SignUpProfileActivity.kt
+++ app/src/main/java/com/example/eraofband/signup/SignUpProfileActivity.kt
@@ -48,8 +48,12 @@ class SignUpProfileActivity : AppCompatActivity() {
         }
 
         setTextColor()
-        initImageViewProfile()
-
+        binding.signupProfileAddIv.setOnClickListener{
+            initImageViewProfile()
+        }
+        binding.signupProfileProfileIv.setOnClickListener{
+            initImageViewProfile()
+        }
     }
 
     private fun setTextColor() {
@@ -80,30 +84,27 @@ class SignUpProfileActivity : AppCompatActivity() {
     }
 
     private fun initImageViewProfile() {
-        binding.signupProfileAddIv.setOnClickListener {
-            when {
-                // 갤러리 접근 권한이 있는 경우
-                ContextCompat.checkSelfPermission(
-                    this,
-                    android.Manifest.permission.READ_EXTERNAL_STORAGE
-                ) == PackageManager.PERMISSION_GRANTED
-                -> {
-                    navigateGallery()
-                }
-
-                // 갤러리 접근 권한이 없는 경우 & 교육용 팝업을 보여줘야 하는 경우
-                shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                -> {
-                    showPermissionContextPopup()
-                }
-
-                // 권한 요청 하기(requestPermissions) -> 갤러리 접근(onRequestPermissionResult)
-                else -> requestPermissions(
-                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
-                    1000
-                )
+        when {
+            // 갤러리 접근 권한이 있는 경우
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+            -> {
+                navigateGallery()
             }
 
+            // 갤러리 접근 권한이 없는 경우 & 교육용 팝업을 보여줘야 하는 경우
+            shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            -> {
+                showPermissionContextPopup()
+            }
+
+            // 권한 요청 하기(requestPermissions) -> 갤러리 접근(onRequestPermissionResult)
+            else -> requestPermissions(
+                arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                1000
+            )
         }
     }
 
