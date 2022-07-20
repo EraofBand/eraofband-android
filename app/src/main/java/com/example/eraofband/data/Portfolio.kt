ba9219bbed4770e875fa2de7266e284b91ec36d8
joeun-01 <joeun2187@gmail.com>
2022-07-20 19:47:36 +0900

    modify: [ProfileEditActivity] 프로필 편집 프로필 사진 연동

--- app/src/main/java/com/example/eraofband/data/Portfolio.kt
+++ app/src/main/java/com/example/eraofband/data/Portfolio.kt
@@ -2,7 +2,7 @@ package com.example.eraofband.data
 
 data class Portfolio (
     var content : String,
-    var imgUrl : Int,  // 나중에 String으로 바꿉시당, 이게 프사였네요
+    var imgUrl : String,  // 나중에 String으로 바꿉시당, 이게 프사였네요
     var title : String,
     var userIdx : Int,
     var vidioUrl : String
